657e1623241c65f3074a30fa63164d9167a0bf3f
Yakov Mokin <mokinyakov@mail.ru>
2025-11-26 00:17:02 +0300

    Move heavy compiled deps to extras_require to avoid building on install

--- setup.py
+++ setup.py
@@ -30,7 +30,14 @@ setup(
   long_description_content_type='text/markdown',
   url='https://github.com/skewer33/ProteinNetworks.git',
   packages=find_packages(),
-  install_requires=get_requirements(),
+  # split requirements: keep lightweight essentials in install_requires
+  _reqs = get_requirements()
+  _heavy = {'matplotlib', 'leidenalg', 'igraph', 'umap-learn', 'python-igraph'}
+  install_requires=[r for r in _reqs if r.split('==')[0] not in _heavy],
+  # allow optional installation of heavy/compiled deps via extras, e.g. pip install .[full]
+  extras_require={
+      'full': [r for r in _reqs if r.split('==')[0] in _heavy],
+  },
   classifiers=[
     'Programming Language :: Python :: 3.12',
     'License :: OSI Approved :: MIT License',
